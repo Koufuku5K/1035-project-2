3d813b4fbd1b66d49087119937c078bd17110714
Alfie <a.j.s.fields2@newcastle.ac.uk>
2021-03-10 16:25:57 +0000

    Make one to many with module and booking

--- src/main/java/csc1035/project2/Module.java
+++ src/main/java/csc1035/project2/Module.java
@@ -1,16 +1,36 @@
 package csc1035.project2;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "Module")
 class Module{
+
+    @Id
+    @Column(updatable = false, nullable = false)
     private String moduleID;
+
+    @Column
     private String moduleName;
+
+    @Column
     private int credits;
+
+    @Column
     private int weeks;
 
-    public Module(String moduleID, String moduleName, int credits, int weeks) {
+    @OneToMany(mappedBy = "module")
+    private List<Booking> module;
+
+    public Module(String moduleID, String moduleName, int credits, int weeks, List<Booking> module) {
         this.moduleID = moduleID;
         this.moduleName = moduleName;
         this.credits = credits;
         this.weeks = weeks;
+        this.module = module;
+    }
+
+    public Module(){
     }
 
     public String getModuleID() {
@@ -43,5 +63,13 @@ class Module{
 
     public void setWeeks(int weeks) {
         this.weeks = weeks;
+    }
+
+    public List<Booking> getModule() {
+        return module;
+    }
+
+    public void setModule(List<Booking> module) {
+        this.module = module;
     }
 }
