17ae41f2f8a1baae37bd02be8d51b0c3b33dc997
Kyle <c0010105@newcastle.ac.uk>
2021-03-15 11:13:02 +0000

    Created method to allow user to choose which timetable to view in Timetable.java Created method for each timetable that returns a query

--- src/main/java/csc1035/project2/Timetable.java
+++ src/main/java/csc1035/project2/Timetable.java
@@ -1,5 +1,10 @@
 package csc1035.project2;
-import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Scanner;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 public class Timetable {
     /*Todo
@@ -8,11 +13,76 @@ public class Timetable {
     Method to collect data about modules that student or staff on then collect all sessions for those modules
 
      */
-    public ArrayList returnTimetable(){
-        ArrayList timetable;
-        return timetable;
+    public void returnTimetable(){
+        List timetable;
     }
-    public static void main(String[] args) {
 
+    public void searchDatabase(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Who would you like the timetable for:");
+        System.out.println("1)A Student");
+        System.out.println("2)A member of staff");
+        System.out.println("3)A module");
+        System.out.println("4)A Room");
+        String choice = s.nextLine();
+        if(choice =="1"){
+            studentTimetable();
+        }
+        else if(choice == "2"){
+            staffTimetable();
+        }
+        else if(choice == "3"){
+            moduleTimetable();
+        }
+        else if(choice == "4") {
+            roomTimetable();
+        }
+        else{
+            System.out.println("Invalid Choice");
+            searchDatabase();
+            }
+    }
+
+    public Query studentTimetable(){
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        s.beginTransaction();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the student ID: ");
+        String studentID = input.nextLine();
+        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber, m.moduleName, b.duration from Student s where s.userID = "+studentID+"from Student s");
+        return query;
+    }
+
+    public Query staffTimetable(){
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        s.beginTransaction();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the staff ID: ");
+        String staffID = input.nextLine();
+        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber, m.moduleName, b.duration from Staff s where s.userID = "+staffID+"from Staff s");
+        return query;
+    }
+
+    public Query moduleTimetable(){
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        s.beginTransaction();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the module ID: ");
+        String moduleID = input.nextLine();
+        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber,m.moduleID, m.moduleName, b.duration from Module m where m.moduleID = "+moduleID+"from Module m");
+        return query;
+    }
+
+    public Query roomTimetable(){
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        s.beginTransaction();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the room number: ");
+        String roomNumber = input.nextLine();
+        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber,m.moduleID, m.moduleName, b.duration from Module m where b.roomNumber = "+roomNumber+"from Module m");
+        return query;
+    }
+
+    public static void main(String[] args) {
     }
 }
