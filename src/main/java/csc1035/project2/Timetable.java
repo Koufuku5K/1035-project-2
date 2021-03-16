44fbdef0fe73f12c0b6bd67b01dc7cade8b1e6cb
Joe <J.Burley1@newcastle.ac.uk>
2021-03-16 08:51:58 +0000

    Insert HQL queries

--- src/main/java/csc1035/project2/Timetable.java
+++ src/main/java/csc1035/project2/Timetable.java
@@ -17,7 +17,10 @@ public class Timetable {
         List timetable;
     }
 
-    public void searchDatabase(){
+    public static Query selectQuery(){
+
+        Query query = null;
+
         Scanner s = new Scanner(System.in);
         System.out.println("Who would you like the timetable for:");
         System.out.println("1)A Student");
@@ -25,64 +28,78 @@ public class Timetable {
         System.out.println("3)A module");
         System.out.println("4)A Room");
         String choice = s.nextLine();
-        if(choice =="1"){
-            studentTimetable();
-        }
-        else if(choice == "2"){
-            staffTimetable();
-        }
-        else if(choice == "3"){
-            moduleTimetable();
-        }
-        else if(choice == "4") {
-            roomTimetable();
-        }
-        else{
+
+        if(choice == "1"){
+            query = studentTimetable();
+        } else if(choice == "2") {
+            query = staffTimetable();
+        } else if(choice == "3"){
+            query = moduleTimetable();
+        } else if(choice == "4") {
+            query = roomTimetable();
+        } else{
             System.out.println("Invalid Choice");
-            searchDatabase();
+            selectQuery();
             }
+
+        return query;
+
     }
 
-    public Query studentTimetable(){
+    public static Query studentTimetable(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the student ID: ");
         String studentID = input.nextLine();
-        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber, m.moduleName, b.duration from Student s where s.userID = "+studentID+"from Student s");
+        Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Student s JOIN Teach t ON s.userID=t.userID JOIN Module m ON t.moduleID=m.moduleID JOIN Booking b ON m.moduleID=b.moduleID Where s.userID =" + studentID);
         return query;
     }
 
-    public Query staffTimetable(){
+    public static Query staffTimetable(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the staff ID: ");
         String staffID = input.nextLine();
-        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber, m.moduleName, b.duration from Staff s where s.userID = "+staffID+"from Staff s");
+        Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Staff s JOIN Teach t ON s.userID=t.userID JOIN Module m ON t.moduleID=m.moduleID JOIN Booking b ON m.moduleID=b.moduleID WHERE s.userID =" + staffID);
         return query;
     }
 
-    public Query moduleTimetable(){
+    public static Query moduleTimetable(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the module ID: ");
         String moduleID = input.nextLine();
-        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber,m.moduleID, m.moduleName, b.duration from Module m where m.moduleID = "+moduleID+"from Module m");
+        Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Module m JOIN Booking b ON m.moduleID = b.moduleID WHERE m.moduleID =" + moduleID);
         return query;
     }
 
-    public Query roomTimetable(){
+    public static Query roomTimetable(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the room number: ");
         String roomNumber = input.nextLine();
-        Query query = s.createQuery("select b.date, b.startTime, b.roomNumber,m.moduleID, m.moduleName, b.duration from Module m where b.roomNumber = "+roomNumber+"from Module m");
+        Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Module m JOIN Booking b ON m.moduleID=b.moduleID WHERE b.roomNumber = Variable" + roomNumber);
         return query;
     }
 
     public static void main(String[] args) {
+        Query query = selectQuery();
+
+        outputQuery(query);
     }
+
+    public static void outputQuery(Query query){
+
+        List list = query.list();
+        System.out.println(list);
+
+
+
+    }
+
+
 }
