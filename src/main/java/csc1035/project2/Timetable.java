931ec98e2025bb5d95d243346996b0726b6b96c7
Alfie <a.j.s.fields2@newcastle.ac.uk>
2021-03-19 15:17:10 +0000

    Rework methods to output String instead of Objects

--- src/main/java/csc1035/project2/Timetable.java
+++ src/main/java/csc1035/project2/Timetable.java
@@ -1,27 +1,14 @@
 package csc1035.project2;
-import java.util.List;
-import javax.persistence.*;
-import java.util.Objects;
-import java.util.Scanner;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
 public class Timetable {
-    /*Todo
-    Method to return timetable
-    Needs to return timetable for ID of student or staff
-    Method to collect data about modules that student or staff on then collect all sessions for those modules
+    private static final Scanner input = new Scanner(System.in);
 
-     */
-    public void returnTimetable(){
-        List timetable;
-    }
-
-    public static Query selectQuery(){
-
-        Query query = null;
-
-        Scanner s = new Scanner(System.in);
+    public static void selectQuery(){
         System.out.println("What would you like to see");
         System.out.println("1)Student Timetable");
         System.out.println("2)Staff Timetable");
@@ -29,53 +16,93 @@ public class Timetable {
         System.out.println("4)Room Time table");
         System.out.println("5)List of students on Module");
         System.out.println("6)List of staff on Module");
-        String choice = s.nextLine();
+        String choice = input.nextLine();
 
-        if(choice.equals("1")){
-            query = studentTimetable();
-        } else if(choice.equals("2")){
-            query = staffTimetable();
-        } else if(choice.equals("3")){
-            query = moduleTimetable();
-        } else if(choice.equals("4")){
-            query = roomTimetable();
-        } else if(choice.equals("5")){
-            query = takesModule();
-        } else if(choice.equals("6")){
-            query = teachesModule();
-        } else{
-            System.out.println("Invalid Choice");
-            selectQuery();
+        switch (choice) {
+            case "1" -> studentTimetable();
+            case "2" -> staffTimetable();
+            case "3" -> moduleTimetable();
+            case "4" -> roomTimetable();
+            case "5" -> takesModule();
+            case "6" -> teachesModule();
+            default -> {
+                System.out.println("Invalid Choice");
+                selectQuery();
             }
-
-        return query;
-
+        }
     }
 
-    public static Query studentTimetable(){
+    public static void studentTimetable(){
+        List<Booking> classesEnrolledIn = new ArrayList<>();
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
-        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the student ID: ");
         String studentID = input.nextLine();
-        Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Student s JOIN Teach t ON s.userID=t.userID JOIN Module m ON t.moduleID=m.moduleID JOIN Booking b ON m.moduleID=b.moduleID Where s.userID =" + studentID);
-        return query;
+        Students student = s.get(Students.class, studentID);
+        for (Module m: student.getModules()) {
+            Query query = s.createQuery("FROM Booking b WHERE b.moduleID = :moduleID");
+            query.setParameter("moduleID", m);
+            classesEnrolledIn.addAll(query.list());
+        }
+
+        for (Booking b: classesEnrolledIn) {
+            // Extracts the bit of the date from the Calendar object we want, and format it
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date dateToParse = b.getDate().getTime();
+            String date = dateFormat.format(dateToParse);
+
+            // Extracts the bit of the time from the Calendar object we want, and format it
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+            Date timeToParse = b.getStartTime().getTime();
+            String time = timeFormat.format(timeToParse);
+
+            System.out.println("Session");
+            System.out.println("Room: " + (b.getRoomNumber()).getRoomNumber());
+            System.out.println("Module: " + (b.getModuleID()).getModuleID());
+            System.out.println("Social Distancing: " + b.getIsSociallyDistant());
+            System.out.println("Date: " + date);
+            System.out.println("Time: " + time);
+            System.out.println("Duration: " + b.getDuration() + "\n");
+        }
     }
 
-    public static Query staffTimetable(){
+    public static void staffTimetable(){
+        List<Booking> modulesTaught = new ArrayList<>();
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
-        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the staff ID: ");
         String staffID = input.nextLine();
-        Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Staff s JOIN Teach t ON s.userID=t.userID JOIN Module m ON t.moduleID=m.moduleID JOIN Booking b ON m.moduleID=b.moduleID WHERE s.userID =" + staffID);
-        return query;
+        Staff staff = s.get(Staff.class, staffID);
+        for (Module m: staff.getModules()) {
+            Query query = s.createQuery("FROM Booking b WHERE b.moduleID = :moduleID");
+            query.setParameter("moduleID", m);
+            modulesTaught.addAll(query.list());
+        }
+
+        for (Booking b: modulesTaught) {
+            // Extracts the bit of the date from the Calendar object we want, and format it
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date dateToParse = b.getDate().getTime();
+            String date = dateFormat.format(dateToParse);
+
+            // Extracts the bit of the time from the Calendar object we want, and format it
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+            Date timeToParse = b.getStartTime().getTime();
+            String time = timeFormat.format(timeToParse);
+
+            System.out.println("Session");
+            System.out.println("Room: " + (b.getRoomNumber()).getRoomNumber());
+            System.out.println("Module: " + (b.getModuleID()).getModuleID());
+            System.out.println("Social Distancing: " + b.getIsSociallyDistant());
+            System.out.println("Date: " + date);
+            System.out.println("Time: " + time);
+            System.out.println("Duration: " + b.getDuration() + "\n");
+        }
     }
 
     public static Query moduleTimetable(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
-        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the module ID: ");
         String moduleID = input.nextLine();
         Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Module m INNER JOIN Booking b ON m.moduleID = b.moduleID WHERE m.moduleID =" + moduleID);
@@ -85,48 +112,35 @@ public class Timetable {
     public static Query roomTimetable(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
-        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the room number: ");
         String roomNumber = input.nextLine();
         Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Module m INNER JOIN Booking b ON m.moduleID=b.moduleID WHERE b.roomNumber = Variable" + roomNumber);
         return query;
     }
 
-    public static Query takesModule(){
+    public static void takesModule(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
-        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the module ID: ");
         String moduleID = input.nextLine();
-        Query query = s.createQuery("SELECT b.date, b.startTime, b.roomNumber, m.moduleID, m.moduleName, b.duration FROM Student s INNER JOIN Teach t ON s.userID=t.userID INNER JOIN Module m ON t.moduleID=m.moduleID INNER JOIN Booking b ON m.moduleID=b.moduleID Where s.userID =" + moduleID);
-        return query;
+        Module module = s.get(Module.class, moduleID);
+        for (Students stud: module.getStudents()) {
+            System.out.println(stud);
+        }
     }
 
-    public static Query teachesModule(){
+    public static void teachesModule(){
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
-        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the module ID: ");
         String moduleID = input.nextLine();
-        Query query = s.createQuery("SELECT u FROM Take t INNER JOIN Student s ON t.userID=s.userID INNER JOIN User u ON s.userID=u.userID WHERE r.moduleID = " + moduleID);
-        return query;
+        Module module = s.get(Module.class, moduleID);
+        for (Staff staff: module.getStaff()) {
+            System.out.println(staff);
+        }
     }
-
 
     public static void main(String[] args) {
-        Query query = selectQuery();
-
-        outputQuery(query);
+        selectQuery();
     }
-
-    public static void outputQuery(Query query){
-
-        List list = query.list();
-        System.out.println(list);
-
-
-
-    }
-
-
 }
